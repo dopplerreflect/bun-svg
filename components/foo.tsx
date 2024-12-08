9b3ed08e66f91706640cb775ff3924526fb1da05
David Rose <doppler@gmail.com>
2024-12-08 13:51:27 -0600

    refactor

---
+++ components/foo.tsx
@@ -0,0 +1,109 @@
+import { anglesArray, radialPointString, PHI } from "geometry";
+
+import chroma from "chroma-js";
+
+const oklch = (l: number, c: number, h: number): string =>
+  chroma.oklch(l, c, h).hex();
+
+const phi = PHI - 1;
+
+const Group = (props: React.SVGProps<SVGGElement>) => {
+  const { children, ...rest } = props;
+  return <g {...rest}>{children}</g>;
+};
+
+type Props = {
+  width?: number;
+  height?: number;
+  strokeSize?: number;
+};
+const Foo = ({ width = 1920, height = 1080, strokeSize = 1 }: Props) => {
+  const hue = 330;
+  const radius = (height / 2) * phi;
+  const angles = anglesArray(5);
+  const Star = () => (
+    <polygon
+      points={angles
+        .map((_, i) =>
+          radialPointString(angles[(i * 2) % angles.length], radius),
+        )
+        .join(" ")}
+    />
+  );
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
+    >
+      <defs>
+        <linearGradient
+          id='backgroundGradient'
+          gradientTransform='rotate(90)'
+        >
+          <stop
+            stopColor={oklch(0.5, 0.37, hue)}
+            offset='0%'
+          />
+          <stop
+            stopColor={oklch(0.0, 0.37, hue - 60)}
+            offset='100%'
+          />
+        </linearGradient>
+        <filter id='highlight'>
+          <feGaussianBlur
+            in='SourceAlpha'
+            stdDeviation={5 * strokeSize}
+          />
+          <feOffset
+            dy={5 * strokeSize}
+            result='shadow'
+          />
+          <feMorphology
+            in='SourceGraphic'
+            operator='erode'
+            radius={4 * strokeSize}
+          />
+          <feColorMatrix
+            values='
+            1 0 0 0 0.5
+            0 1 0 0 0.5
+            0 0 1 0 0.5
+            0 0 0 1 0
+          '
+          />
+          <feGaussianBlur stdDeviation={2 * strokeSize} />
+          <feOffset
+            dy={-5 * strokeSize}
+            result='highlight'
+          />
+          <feMerge>
+            <feMergeNode in='shadow' />
+            <feMergeNode in='SourceGraphic' />
+            <feMergeNode in='highlight' />
+          </feMerge>
+        </filter>
+      </defs>
+      <path
+        d={`M${-width / 2},${-height / 2}H${width / 2}V${height / 2}H${
+          -width / 2
+        }Z`}
+        // fill={oklch(0.15, 0.37, hue)}
+        fill='url(#backgroundGradient)'
+      />
+      <Group
+        filter='url(#highlight)'
+        strokeWidth={20 * strokeSize}
+        stroke={oklch(0.5, 0.37, hue)}
+        strokeLinejoin='round'
+        fill='none'
+      >
+        <>
+          <circle r={radius} />
+          <Star />
+        </>
+      </Group>
+    </svg>
+  );
+};
+
+export default Foo;
--- index.tsx
+++ index.tsx
@@ -1,114 +1,23 @@
 import { renderToStaticMarkup } from "react-dom/server";
 import { $ } from "bun";
-import { PHI, anglesArray, radialPointString } from "geometry";
-import type { ReactElement } from "react";
-import chroma from "chroma-js";
+import SVG from "./components/foo";
 
-const oklch = (l: number, c: number, h: number): string =>
-  chroma.oklch(l, c, h).hex();
+const filePath = `./images/${SVG.name}`;
 
-const phi = PHI - 1;
+const svg = renderToStaticMarkup(<SVG />);
 
-type GroupProps = {
-  filter: string;
-  strokeWidth: number;
-  children: ReactElement;
-};
+await Bun.write(`${filePath}.svg`, svg);
 
-const Group = ({ children, filter, strokeWidth }: GroupProps) => {
-  return (
-    <g
-      filter={filter}
-      strokeWidth={strokeWidth}
-    >
-      {children}
-    </g>
-  );
-};
-const Foo = () => {
-  const width = 1920;
-  const height = 1080;
-  const hue = 270;
-  const saturation = "100%";
-  const radius = (height / 2) * phi;
-  const angles = anglesArray(5);
-  const starPoints = angles
-    .map((_, i) => radialPointString(angles[(i * 2) % angles.length], radius))
-    .join(" ");
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
-    >
-      <defs>
-        <filter id='highlight'>
-          <feGaussianBlur
-            in='SourceAlpha'
-            stdDeviation={5}
-          />
-          <feOffset
-            dy={5}
-            result='shadow'
-          />
-          <feMorphology
-            in='SourceGraphic'
-            operator='erode'
-            radius={4}
-          />
-          <feColorMatrix
-            values='
-            1 0 0 0 0.5
-            0 1 0 0 0.5
-            0 0 1 0 0.5
-            0 0 0 1 0
-          '
-          />
-          <feGaussianBlur stdDeviation={2} />
-          <feOffset
-            dy={-5}
-            result='highlight'
-          />
-          <feMerge>
-            <feMergeNode in='shadow' />
-            <feMergeNode in='SourceGraphic' />
-            <feMergeNode in='highlight' />
-          </feMerge>
-        </filter>
-      </defs>
-      <path
-        d={`M${-width / 2},${-height / 2}H${width / 2}V${height / 2}H${
-          -width / 2
-        }Z`}
-        fill={`hsla(${hue - 60}, 50%, 25%, 1)`}
-      />
-      <Group
-        filter='url(#highlight)'
-        strokeWidth={20}
-      >
-        <>
-          <circle
-            r={(height / 2) * 0.618}
-            stroke={oklch(0.5, 0.37, 300)}
-            fill='none'
-          />
-          <polygon
-            points={starPoints}
-            fill='none'
-            strokeLinejoin='round'
-            stroke={oklch(0.5, 0.37, 300)}
-          />
-        </>
-      </Group>
-    </svg>
-  );
-};
+async function renderToPNG() {
+  const { stdout, stderr, exitCode } =
+    await $`magick -background none ${filePath}.svg ${filePath}.png`;
+  if (exitCode) console.log(stderr.toString());
+}
+async function setDesktopBackground() {
+  const { stdout, stderr, exitCode } =
+    await $`swww img -o eDP-1 -t top --transition-duration 1 ${filePath}.png`;
+  if (exitCode) console.log(stderr.toString());
+}
 
-const IMAGES_DIR = "./images";
-
-const svg = renderToStaticMarkup(<Foo />);
-
-await Bun.write(`${IMAGES_DIR}/current.svg`, svg);
-const { stdout, stderr, exitCode } =
-  await $`magick -background none ${IMAGES_DIR}/current.svg ${IMAGES_DIR}/current.png`;
-
-if (exitCode) console.log(stderr.toString());
+await renderToPNG();
+await setDesktopBackground();
