9260f83121a7a8bd6f2b06524db316018cbb5339
David Rose <doppler@gmail.com>
2024-12-09 01:43:27 -0600

    matrix: all lines and added gradients.

--- src/drawings/matrix/index.tsx
+++ src/drawings/matrix/index.tsx
@@ -1,6 +1,6 @@
 import { Background } from "$components/all";
 import { oklch } from "$lib/color";
-import { anglesArray, PHI, radialPoint, radialPointString } from "geometry";
+import { anglesArray, PHI, radialPoint } from "geometry";
 
 type Props = {
   width?: number;
@@ -10,6 +10,37 @@ export default function Matrix({ width = 1920, height = 1080 }: Props) {
   const phi = PHI - 1;
   const angles = anglesArray(10);
   const radii = [...Array(5).keys()].map(i => (height / 2) * 0.95 * phi ** i);
+
+  const lineAngleMatrixSet: Set<string> = new Set();
+
+  angles.forEach((_, a) => {
+    [...Array(7).keys()].forEach(b => {
+      lineAngleMatrixSet.add(
+        JSON.stringify([a, (a + b + 2) % angles.length].sort()),
+      );
+    });
+  });
+  const lineAngleMatrix = [...lineAngleMatrixSet].map(e => JSON.parse(e));
+
+  const Lines = () =>
+    radii.map(radius =>
+      lineAngleMatrix.map((e, i) => {
+        const [start, end] = [
+          radialPoint(angles[e[0]], radius),
+          radialPoint(angles[e[1]], radius),
+        ];
+        return (
+          <line
+            key={i}
+            x1={start[0]}
+            y1={start[1]}
+            x2={end[0]}
+            y2={end[1]}
+            stroke={oklch(0.95, 0.5, 270)}
+          />
+        );
+      }),
+    );
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -27,45 +58,53 @@ export default function Matrix({ width = 1920, height = 1080 }: Props) {
             <feMergeNode in='SourceGraphic' />
           </feMerge>
         </filter>
+        {radii.map((radius, i) => {
+          return (
+            <radialGradient
+              id={`gradient${i}`}
+              key={i}
+            >
+              <stop
+                offset='0%'
+                stopColor={oklch(1, 0.15, 90)}
+              />
+              <stop
+                offset='61.8%'
+                stopColor={oklch(0, 0.15, 270)}
+              />
+              <stop
+                offset='100%'
+                stopColor={oklch(0.0, 0.5, 270)}
+              />
+            </radialGradient>
+          );
+        })}
       </defs>
       <Background
         {...{ width, height }}
-        fill={oklch(0.05, 0.2, 300)}
+        fill={oklch(0.0, 0.15, 300)}
       />
       <g filter='url(#cheap-glow)'>
-        {radii.map(r => (
+        {radii.map((r, i) => (
+          <circle
+            key={r}
+            r={r}
+            stroke={oklch(0.95, 0.8, 270)}
+            fill={`url(#gradient${i})`}
+          />
+        ))}
+        {radii.map((r, i) => (
           <circle
             key={r}
             r={r}
             stroke={oklch(0.95, 0.8, 270)}
             fill='none'
+            filter='url(#cheap-glow)'
           />
         ))}
-        {radii.map(r => (
-          <g
-            key={r}
-            id={r.toString()}
-          >
-            {angles.slice(0, 9).map((a, outerIdx) => {
-              const start = radialPoint(a, r);
-              return [...Array(7).keys()].map(innerIdx => {
-                const end = radialPoint(
-                  angles[(outerIdx + innerIdx + 2) % angles.length],
-                  r,
-                );
-                return (
-                  <line
-                    x1={start[0]}
-                    y1={start[1]}
-                    x2={end[0]}
-                    y2={end[1]}
-                    stroke={oklch(0.95, 0.5, 270)}
-                  />
-                );
-              });
-            })}
-          </g>
-        ))}
+        <g filter='url(#cheap-glow)'>
+          <Lines />
+        </g>
       </g>
     </svg>
   );
