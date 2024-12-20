e77042fd60514e775b31c8b98c61bf1eba1f3b69
David Rose <doppler@gmail.com>
2024-12-19 22:32:52 -0600

    add decagonal star crystal and golden circles to phyllo bg

--- src/drawings/phylotaxis/index.tsx
+++ src/drawings/phylotaxis/index.tsx
@@ -3,11 +3,19 @@ import {
   phylotaxis,
   findNearest,
   calculateDistance,
+  chordMatrix,
+  anglesArray,
+  phi,
+  radialPoint,
+  PHI,
 } from "@dopplerreflect/geometry";
 import Background from "$components/Background";
 import { oklch } from "chroma-js";
 
-export default function Phylotaxis({ width = 1920, height = 1080 }) {
+export default function GoldenCrystalPhyllotaxic({
+  width = 1920,
+  height = 1080,
+}) {
   const count = 2 ** 12;
   const phylotaxicRadius = Math.sqrt((width / 2) ** 2 + (height / 2) ** 2);
   const phylotaxicPoints = phylotaxis(count, phylotaxicRadius).filter(
@@ -26,6 +34,19 @@ export default function Phylotaxis({ width = 1920, height = 1080 }) {
       phylotaxicCircles.push({ r: distance - nearestCircle.r, ...p });
     }
   });
+
+  const angles = anglesArray(10);
+  const radii = [...Array(3).keys()].map(i => (height / 2) * 0.35 * phi ** i);
+
+  const circles: Circle[] = [
+    ...radii.map(r => ({ r, ...{ x: 0, y: 0 } })),
+    ...angles.map(a => radii.map(r => ({ r, ...radialPoint(a, radii[0]) }))),
+  ].flat();
+
+  const lines = chordMatrix(
+    angles,
+    radii.slice(0, 3).map(r => r * PHI ** 2),
+  );
 
   return (
     <svg
@@ -51,7 +72,35 @@ export default function Phylotaxis({ width = 1920, height = 1080 }) {
             radius={3}
             operator={"dilate"}
           />
-          <feGaussianBlur stdDeviation={3} />
+          <feGaussianBlur stdDeviation={"5 3"} />
+          <feMerge>
+            <feMergeNode />
+            <feMergeNode in='SourceGraphic' />
+          </feMerge>
+        </filter>
+        <filter id='glow'>
+          <feMorphology
+            operator={"dilate"}
+            radius={0}
+          />
+          <feGaussianBlur stdDeviation={5} />
+          <feComponentTransfer>
+            <feFuncR
+              type='linear'
+              slope={1.5}
+              intercept={0.2}
+            />
+            <feFuncG
+              type='linear'
+              slope={1.5}
+              intercept={0.2}
+            />
+            <feFuncB
+              type='linear'
+              slope={1.5}
+              intercept={0.2}
+            />
+          </feComponentTransfer>
           <feMerge>
             <feMergeNode />
             <feMergeNode in='SourceGraphic' />
@@ -62,27 +111,62 @@ export default function Phylotaxis({ width = 1920, height = 1080 }) {
         {...{ width, height }}
         fill={oklch(0.0, 0.37, 300).hex()}
       />
-      <g filter='url(#blur)'>
-        {phylotaxicCircles.map((c, i) => (
+      <g>
+        <g
+          id='phylo-circles'
+          filter='url(#blur)'
+          opacity={0.33}
+        >
+          {phylotaxicCircles.map((c, i) => (
+            <circle
+              key={i}
+              r={c.r}
+              cx={c.x}
+              cy={c.y}
+              fill={oklch(0.15, 0.37, (360 / count) * i).hex()}
+              stroke={oklch(0.99, 0.37, (360 / count) * i).hex()}
+            />
+          ))}
+        </g>
+        <g
+          id='phylo-dots'
+          filter='url(#blur2)'
+        >
+          {phylotaxicCircles.map((p, i) => (
+            <circle
+              key={i}
+              r={p.r / 8}
+              cx={p.x}
+              cy={p.y}
+              fill={oklch(1, 0.27, (360 / count) * i).hex()}
+              transform={`translate(0 -${p.r * phi})`}
+            />
+          ))}
+        </g>
+      </g>
+      <g filter='url(#glow)'>
+        {circles.map((c, i) => (
           <circle
             key={i}
             r={c.r}
             cx={c.x}
             cy={c.y}
-            fill={oklch(0.15, 0.37, (360 / count) * i).hex()}
-            stroke={oklch(0.99, 0.37, (360 / count) * i).hex()}
+            fill='none'
+            stroke={oklch(1, 0.37, 90).hex()}
+            strokeWidth={2}
           />
         ))}
       </g>
-      <g filter='url(#blur2)'>
-        {phylotaxicPoints.map((p, i) => (
-          <circle
+      <g filter='url(#glow)'>
+        {lines.map((l, i) => (
+          <line
             key={i}
-            r={1.5}
-            cx={p.x}
-            cy={p.y}
-            // fill='white'
-            fill={oklch(1, 0.12, (360 / count) * i).hex()}
+            x1={l[0].x}
+            y1={l[0].y}
+            x2={l[1].x}
+            y2={l[1].y}
+            stroke={oklch(1, 0.37, 240).hex()}
+            strokeWidth={2}
           />
         ))}
       </g>
