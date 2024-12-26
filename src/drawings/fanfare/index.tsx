0e696256d1f2dc490b76aedffe50eeb4ee347cd5
David Rose <doppler@gmail.com>
2024-12-26 13:55:15 -0600

    fanfare: add phylo stars.

--- src/drawings/fanfare/index.tsx
+++ src/drawings/fanfare/index.tsx
@@ -2,11 +2,17 @@ import Background from "$components/Background";
 import RadialGradient from "$components/gradients/RadialGradient";
 import {
   anglesArray,
+  calculateDistance,
   chordLength,
   chordMatrix,
   degreesToRadians,
+  findNearest,
   goldenCircles,
   goldenRadii,
+  phylotaxis,
+  radialPoint,
+  starPoints,
+  type Circle,
 } from "@dopplerreflect/geometry";
 import { oklch } from "chroma-js";
 
@@ -19,7 +25,21 @@ export default function Fanfare({ width = 1920, height = 1080 }: Props) {
   const radii = goldenRadii((height / 4) * 0.95, 4);
   const angles = anglesArray(count);
   const circles = goldenCircles(radii, angles);
-
+  const phylo = phylotaxis(
+    2 ** 12,
+    Math.sqrt((width / 2) ** 2 + (height / 2) ** 2),
+  ).filter(
+    p =>
+      Math.sqrt(p.x ** 2 + p.y ** 2) > 1 &&
+      Math.abs(p.x) < width / 2 + width * 0.03 &&
+      Math.abs(p.y) < height / 2 + height * 0.03,
+  );
+  const phyloCircles: Circle[] = [{ r: 1, ...phylo[0] }];
+  phylo.forEach(p => {
+    const nearest = findNearest(p, phyloCircles) as Circle;
+    const distance = calculateDistance(p, nearest);
+    phyloCircles.push({ r: distance - nearest.r, ...p });
+  });
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -35,7 +55,7 @@ export default function Fanfare({ width = 1920, height = 1080 }: Props) {
             { offset: 0, stopColor: oklch(1, 0.37, 90).hex() },
             { offset: 10, stopColor: oklch(1, 0.37, 30).hex() },
             { offset: 20, stopColor: oklch(0.35, 0.37, 300).hex() },
-            { offset: 100, stopColor: oklch(0, 0.0, 300).hex() },
+            { offset: 100, stopColor: oklch(0.15, 0.27, 300).hex() },
           ]}
         />
         <filter id='glow'>
@@ -71,6 +91,35 @@ export default function Fanfare({ width = 1920, height = 1080 }: Props) {
         {...{ width, height }}
         fill='url(#bg-gradient)'
       />
+      <g
+        id='phylo'
+        filter='url(#glow)'
+      >
+        {phyloCircles.map((c, i) => (
+          <polygon
+            key={i}
+            points={starPoints(c)}
+            fill={oklch(1, 0, 0).alpha(0.5).hex()}
+            stroke='none'
+          />
+        ))}
+      </g>
+      <g
+        id='darken'
+        style={{ fillOpacity: 0.5 }}
+      >
+        {angles.map((a, i) => {
+          const c: Circle = { r: radii[0], ...radialPoint(a, radii[0]) };
+          return (
+            <circle
+              key={i}
+              r={c.r}
+              cx={c.x}
+              cy={c.y}
+            />
+          );
+        })}
+      </g>
       <g style={{ display: "inline" }}>
         <g filter='url(#glow)'>
           {radii.map((r, i) =>
