8bd9aa19e9021e04b1a60e4e16dd5afb1be35cb0
David Rose <doppler@gmail.com>
2025-01-28 14:26:36 -0600

    acidflower updates

--- src/drawings/acidflower/index.tsx
+++ src/drawings/acidflower/index.tsx
@@ -1,3 +1,4 @@
+import Background from "$components/Background";
 import {
   phylotaxis,
   findNearest,
@@ -45,21 +46,37 @@ export default function AcidFlower({ width = 1920, height = 1080 }: Props) {
       xmlns='http://www.w3.org/2000/svg'
       viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
     >
-      {phylotaxicCircles.map((c, i) => (
-        <circle
-          key={i}
-          cx={c.x}
-          cy={c.y}
-          r={c.r - 3}
-          stroke={oklch(0.9, 0.37, hues[c.i % hues.length] + i).hex()}
-          strokeWidth={2}
-          fill={
-            i % 3 === 0
-              ? oklch(0.25, 0.37, hues[c.i % hues.length] + 270 + i).hex()
-              : "none"
-          }
-        />
-      ))}
+      <defs>
+        <filter id='glow'>
+          <feGaussianBlur stdDeviation={5} />
+          <feMerge>
+            <feMergeNode />
+            <feMergeNode />
+            <feMergeNode in='SourceGraphic' />
+          </feMerge>
+        </filter>
+      </defs>
+      <Background
+        {...{ width, height }}
+        fill={oklch(0.01, 0.37, 300).hex()}
+      />
+      <g filter='url(#glow)'>
+        {phylotaxicCircles.map((c, i) => (
+          <circle
+            key={i}
+            cx={c.x}
+            cy={c.y}
+            r={c.r - 3}
+            stroke={oklch(0.9, 0.37, hues[c.i % hues.length] + i).hex()}
+            strokeWidth={2}
+            fill={
+              i % 3 === 0
+                ? oklch(0.33, 0.37, hues[c.i % hues.length] + 270 + i).hex()
+                : "none"
+            }
+          />
+        ))}
+      </g>
     </svg>
   );
 }
