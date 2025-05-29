ec248cb69d8dacf77ed6e7bac7c41e7b99949509
David Rose <doppler@gmail.com>
2025-05-29 13:35:10 -0500

    gradients

--- src/drawings/phinest-star/index.tsx
+++ src/drawings/phinest-star/index.tsx
@@ -5,12 +5,14 @@ import { oklch } from "chroma-js";
 import {
   anglesArray,
   arrayMap,
+  lineIntersection,
   midpoint,
   phi,
-  pointToString,
   polygonFromIntersectionOfLines,
   radialPoint,
+  type Circle,
   type Line,
+  type Point,
 } from "@dopplerreflect/geometry";
 
 const templates = products.mensSportsJerseyAOP.templates;
@@ -18,25 +20,20 @@ const templates = products.mensSportsJerseyAOP.templates;
 const originalWidth = templates.front.width;
 const originalHeight = templates.front.height;
 
-const radius = (originalWidth / 2) * 0.43;
+const radius = originalWidth / 2; //* 0.43;
 
 const radii = arrayMap(7, n => radius * phi ** n);
+const circles: Circle[] = radii.map(r => ({ x: 0, y: 0, r }));
 const angles = anglesArray(30);
 
-const Circles = () => (
-  <g id='circles'>
-    {radii.map((r, i) => (
-      <circle
-        key={i}
-        r={r}
-        fill={oklch(0.9, 0.37, 30 + (90 / radii.length) * i).hex()}
-        stroke={"white"}
-        fillOpacity={0.25}
-      />
-    ))}
-  </g>
+const ShrinkFilter = () => (
+  <filter id='shrink'>
+    <feMorphology
+      operator='erode'
+      radius={5}
+    />
+  </filter>
 );
-
 const linesForAStar = (startRadiusIdx: number, startAngleIdx: number): Line[] =>
   [...Array(5).keys()]
     .map(i => [
@@ -77,41 +74,79 @@ const starLineGroups: Line[][] = starStartingPoints.map(a => {
   return linesForAStar(startRadius, startAngle);
 });
 
-const starPolygons = starLineGroups.map(a =>
-  a.map(l => pointToString(l[0])).join(" "),
-);
+const starPolygons = starLineGroups.map(a => a.map(l => l[0]));
 
 const allLines = starLineGroups.flat();
 
 const polygonGroups = [
+  [polygonFromIntersectionOfLines([0, 19, 28, 9, 0], allLines)],
+  [polygonFromIntersectionOfLines([19, 28, 39, 48, 19], allLines)],
+  [polygonFromIntersectionOfLines([38, 39, 48, 49, 59, 58, 38], allLines)],
+
   [
-    polygonFromIntersectionOfLines([0, 19, 28, 9, 0], allLines),
-    polygonFromIntersectionOfLines([0, 1, 29, 10, 0], allLines),
-  ],
-  [
-    polygonFromIntersectionOfLines([27, 47, 18, 27], allLines),
     polygonFromIntersectionOfLines([27, 28, 19, 48, 27], allLines),
     polygonFromIntersectionOfLines([19, 10, 39, 28, 19], allLines),
-    polygonFromIntersectionOfLines([10, 29, 30, 10], allLines),
   ],
+
   [
-    polygonFromIntersectionOfLines([27, 18, 47, 38, 27], allLines),
     polygonFromIntersectionOfLines([47, 48, 19, 18, 47], allLines),
-    polygonFromIntersectionOfLines([19, 28, 39, 48, 19], allLines),
     polygonFromIntersectionOfLines([39, 30, 29, 28, 39], allLines),
   ],
   [
     polygonFromIntersectionOfLines([47, 18, 19, 48, 39, 38, 47], allLines),
     polygonFromIntersectionOfLines([39, 28, 29, 30, 49, 48, 39], allLines),
   ],
-  [polygonFromIntersectionOfLines([38, 39, 48, 49, 59, 58, 38], allLines)],
+  [polygonFromIntersectionOfLines([0, 1, 29, 10, 0], allLines)],
+  [
+    polygonFromIntersectionOfLines([27, 47, 18, 27], allLines),
+    polygonFromIntersectionOfLines([10, 29, 30, 10], allLines),
+  ],
+  [polygonFromIntersectionOfLines([27, 18, 47, 38, 27], allLines)],
 ];
 
+const radiusToPoint = (p: Point): number => Math.hypot(p.x, p.y);
+
+type GradientParams = {
+  r: number;
+  fr: number;
+};
+const gradientParams: GradientParams[] = [
+  { r: radii[0], fr: radii[2] },
+  {
+    r: radii[2],
+    fr: radiusToPoint(lineIntersection(allLines[48], allLines[39]) as Point),
+  },
+  { r: radii[3], fr: radii[5] },
+  {
+    r: radii[1],
+    fr: radiusToPoint(lineIntersection(allLines[48], allLines[19]) as Point),
+  },
+  {
+    r: radii[2],
+    fr: radii[3],
+  },
+  {
+    r: radiusToPoint(lineIntersection(allLines[48], allLines[19]) as Point),
+    fr: radii[4],
+  },
+  {
+    r: radii[1],
+    fr: radiusToPoint(lineIntersection(allLines[10], allLines[29]) as Point),
+  },
+  {
+    r: radii[2],
+    fr: radiusToPoint(lineIntersection(allLines[10], allLines[29]) as Point),
+  },
+  {
+    r: radiusToPoint(lineIntersection(allLines[10], allLines[29]) as Point),
+    fr: radiusToPoint(lineIntersection(allLines[30], allLines[49]) as Point),
+  },
+];
 export default function PhinestStarFront() {
   const { width, height, file } = templates.front;
   const vb = {
     x: -width / 2,
-    y: -height / 2 + height * 0.1,
+    y: -height / 2 + height * 0.0,
     width,
     height,
   };
@@ -122,36 +157,56 @@ export default function PhinestStarFront() {
       viewBox={`${vb.x} ${vb.y} ${vb.width} ${vb.height}`}
       colorInterpolationFilters='sRGB'
     >
+      <defs>
+        <ShrinkFilter />
+        {gradientParams.map((g, i) => (
+          <radialGradient
+            key={i}
+            id={`gradient-${i}`}
+            gradientUnits='userSpaceOnUse'
+            cx={0}
+            cy={0}
+            r={g.r}
+            fr={g.fr}
+          >
+            <stop
+              offset={0}
+              stopColor='black'
+            />
+            <stop
+              offset={1}
+              stopColor={oklch(
+                0.8,
+                0.37,
+                300 + (90 / gradientParams.length) * i,
+              ).hex()}
+              // stopColor='white'
+            />
+          </radialGradient>
+        ))}
+      </defs>
       <Background
         {...{ width, height }}
         vBox={vb}
         fill={oklch(0, 0.19, 300).hex()}
       />
-      <g
-        id='polygons'
-        style={{ display: "" }}
-      >
-        {starPolygons.map((p, i) => (
-          <polygon
-            key={i}
-            points={p}
-            fill={oklch(1, 0, 0).hex()}
-            fillOpacity={0.25}
-          />
-        ))}
-      </g>
       <g id='polygonGroups'>
         {polygonGroups.map((pg, pgi) => (
-          <g key={pgi}>
+          <g
+            key={pgi}
+            fill={`url(#gradient-${pgi})`}
+          >
             {pg.map((p, i) => (
               <polygon
                 key={i}
                 points={p}
-                fill={oklch(
-                  0.85,
+                filter='url(#shrink)'
+                stroke={oklch(
+                  0,
                   0.37,
                   30 + (120 / polygonGroups.length) * pgi,
                 ).hex()}
+                strokeWidth={1}
               />
             ))}
           </g>
@@ -164,8 +219,21 @@ export default function PhinestStarFront() {
           transform={`rotate(${i})`}
         />
       ))}
-      <Circles />
-      <g id='lines'>
+      {circles.map((c, i) => (
+        <circle
+          cx={c.x}
+          cy={c.y}
+          r={c.r}
+          key={i}
+          fill='none'
+          stroke='none'
+          strokeWidth={1}
+        />
+      ))}
+      <g
+        id='allLines'
+        style={{ display: "none" }}
+      >
         {allLines.map((l, i) => (
           <g
             key={i}
@@ -177,8 +245,8 @@ export default function PhinestStarFront() {
               y1={l[0].y}
               x2={l[1].x}
               y2={l[1].y}
-              stroke={"white"}
-              strokeWidth={1}
+              stroke={oklch(0.9, 0.37, 90).hex()}
+              strokeWidth={3}
             />
             <text
               style={{ display: "none" }}
@@ -186,8 +254,9 @@ export default function PhinestStarFront() {
               y={midpoint(l).y}
               textAnchor='middle'
               alignmentBaseline='middle'
-              fill='black'
-              fontSize={24}
+              fill='white'
+              fontSize={64}
+              fontWeight={"bold"}
             >
               {i}
             </text>
